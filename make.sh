6ecb9c9c960e6163899965adae58462fb4a19f5a
Marty Zalega <marty@zalega.me>
2015-09-28 09:27:21 +1000

    Make make make

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 IMAGE_URL=quay.io/everydayhero/buildkite
-TAG=${$TAG:-latest}
+tag=${$TAG:-latest}
 
 bin/download id_rsa
 bin/generate Dockerfile.template Dockerfile
-docker build -t $IMAGE_URL:$TAG .
-docker push $IMAGE_URL:$TAG
+docker build -t $IMAGE_URL:$tag .
+docker push $IMAGE_URL:$tag
