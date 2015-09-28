ae7ee024042b5fac9ceb88d675d09466b0b4bab7
Marty Zalega <marty@zalega.me>
2015-09-28 10:56:14 +1000

    Remove download command

--- make.sh
+++ make.sh
@@ -1,7 +1,6 @@
 IMAGE_URL=quay.io/everydayhero/buildkite
 tag=${TAG:-"latest"}
 
-bin/download id_rsa
 bin/generate Dockerfile.template Dockerfile
 docker build -t $IMAGE_URL:$tag .
 docker push $IMAGE_URL:$tag
